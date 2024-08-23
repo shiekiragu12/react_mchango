bd0eb0416dd12dbd527f3066a55f31f227371b51
Joy Kiragu <shiekiragu123@gmail.com>
2024-08-23 14:17:21 +0300

    update

--- src/auth/Login.js
+++ src/auth/Login.js
@@ -1,8 +1,58 @@
-import React from 'react'
+import React, { useState } from 'react'
+import '../assets/scss/Login.scss'
+import { Col, Container, Form, FormControl, Row, Button,Image } from 'react-bootstrap'
+import LoginImage from '../assets/images/login-pana.png'
 
 function Login() {
+  const [formdata, setFormdata] = useState({
+    email: "",
+    password: "",
+  })
   return (
-    <div>Login</div>
+    <div className='login'>
+      <Container fluid >
+        <Row>
+          <Col md={6} className='sideDisplayData'>
+            <div className='welcomeContent'>
+              <h1 className='welcome'>Welcome</h1>
+              <p className='subheading'>Welcome back! Ready to make a difference? Our platform is here to help you easily manage fundraising for life's important moments, from weddings to business ventures. With seamless mobile payments and instant receipts, everything is simple, transparent, and right at your fingertips no extra apps needed.
+              </p>
+            </div>
+          </Col>
+
+          <Col md={6} className='formLogin'>
+            <Form className='loginFormDisplay'>
+
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Email</Form.Label>
+                <FormControl
+                  type='email'
+                  value={formdata.email}
+                  onChange={(e) => setFormdata({ ...formdata, email: e.target.value })}
+                />
+              </Form.Group>
+
+              <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Password</Form.Label>
+                <FormControl
+                  type='password'
+                  value={formdata.password}
+                  onChange={(e) => setFormdata({ ...formdata, password: e.target.value })}
+                />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                <Form.Check type="checkbox" label="Remember me" />
+              </Form.Group>
+              <Button variant="primary" type="submit">
+                Login
+              </Button>
+            
+              <p className='account'>Don't have an account? <a href='#' className='sign-up'>Sign Up</a></p>
+            </Form>
+          </Col>
+        </Row>
+      </Container>
+    </div >
   )
 }
 
